2cccf1d9e1ae82e308ffe00b2d8d6ff78c740032
OshadhaVimukthi <87273124+OshadhaVimukthiM@users.noreply.github.com>
2021-11-29 16:42:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ def abouts(client, message):
         text=about,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Give Feedback", url="t.me/agentnova")]]
+            [[InlineKeyboardButton("Give Feedback", url="t.me/OshadhaPMbot")]]
         ),
     )
 
