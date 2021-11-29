4d38df0e6090b685189f4c85a77938928fd82b53
OshadhaVimukthi <87273124+OshadhaVimukthiM@users.noreply.github.com>
2021-11-29 16:52:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -127,7 +127,7 @@ def data(client, callback_query):
             text=about,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Give Feedback", url="t.me/agentnova")]]
+                [[InlineKeyboardButton("Give Feedback", url="t.me/OshadhaPMbot")]]
             ),
         )
     elif rslt == "close":
